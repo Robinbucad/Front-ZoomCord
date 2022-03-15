9f2ef56c1b8fbb7faf3b9425a3f156a48a120616
Robinbucad <robin.bucad6@gmail.com>
2022-03-15 07:26:48 +0100

    chat

--- src/components/discordApp/DcChat/index.jsx
+++ src/components/discordApp/DcChat/index.jsx
@@ -1,44 +1,68 @@
 
-import './style.css'
+
 import DivFriend from '../DCmdList'
 import DivServs from '../DCdivServ'
 import { Link } from 'react-router-dom'
 import { useUsername } from '../../../hooks/hook-name-user'
 import { useState } from 'react'
+import classes from './chat.module.scss'
 
 function Chat() {
 
     const username = useUsername()
+    console.log(username)
 
     return (
 
-        <section className="div-app-discord">
+        <section className={classes.chatFriend}>
             <DivServs></DivServs>
             <DivFriend></DivFriend>
-             <section className='chat-container'>
-            <header className="header-chat">
+            <section className={classes.chatContainer}>
+                <header className={classes.headerChat}>
                     <p>{username}</p>
-                <div className='group-header-chat'>
-                    <p>Noti</p>
-                    <p>Members</p>
-                    <input className='input-chat-serv' type='text' placeholder='Buscar'></input>
-                    <p>Help</p>
-                </div>
-            </header>
-
-            <section className='chat'>
-                    <div>
-                        <p>Chat</p>
+                    <div className={classes.groupHeaderChat}>
+                        <p>Noti</p>
+                        <p>Members</p>
+                        <input className={classes.inputChatSearch} type='text' placeholder='Buscar'></input>
+                        <p>Help</p>
                     </div>
-                       
+                </header>
 
-                    <footer className='div-input-chat'>
-                        <input placeholder='Enviar mensaje a #general' className='input-chat' type="text" />
+                <section className={classes.chat}>
+                    <div className={classes.conversation}>
+                        <div className={classes.msg}>
+                            <p>IMG</p>
+                            <div>
+                                <p>Robin</p>
+                                <p>Hola este es mi mensaje</p>
+                            </div>
+                        </div>
+
+                        <div className={classes.msg}>
+                            <p>IMG</p>
+                            <div>
+                                <p>Paco</p>
+                                <p>Hola este es mi mensaje2</p>
+                            </div>
+                        </div>
+
+                        <div className={classes.msg}>
+                            <p>IMG</p>
+                            <div>
+                                <p>Robin</p>
+                                <p>Hola este es mi mensaje3</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <footer className={classes.divInputChat}>
+                        <input placeholder='Enviar mensaje a #general' className={classes.inputChat} type="text" />
                     </footer>
+                </section>
             </section>
         </section>
-        </section>
-       
+
     )
 }
 
