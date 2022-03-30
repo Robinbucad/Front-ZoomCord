8f390e2ebf7dc3a9bceaa59a8569501c2d100266
unknown <robin.bucad6@gmail.com>
2022-03-30 14:53:58 -0700

    lap

--- src/components/HomePage/footer/index.jsx
+++ src/components/HomePage/footer/index.jsx
@@ -44,8 +44,8 @@ function HomeFooter() {
                     <div>
                     {flag === ES ? <img className={classes.imgFlag} src={ES}></img> :  <img className={classes.imgFlag} src={KY}></img>}
                         <select className='select-lng' onChange={handleChange}>
-                            <option className={classes.optSelect} value='es'> Español</option>
-                            <option className={classes.optSelect} value='en'> English</option>
+                            <option className={classes.optSelect} value='es'> Es</option>
+                            <option className={classes.optSelect} value='en'> En</option>
                         </select>
                     </div>
 
